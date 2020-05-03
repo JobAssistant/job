17210c6a5cd9a6e98e1112909eb7fd22562928fa
xoste49 <xoste49@gmail.com>
2020-05-03 12:39:51 +0300

    Реализован запрос Post с массивом string[]

--- MailSenderAPI/Models/Mails.cs
+++ MailSenderAPI/Models/Mails.cs
@@ -1,15 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MailSenderAPI.Models
 {
-    public partial class Mails
-    {
-        public int Id { get; set; }
-        public string Subject { get; set; }
-        public string Body { get; set; }
-        public string[] Recipients { get; set; }
-        public DateTime Datecreate { get; set; }
-        public string Result { get; set; }
-    }
+   public partial class Mails
+   {
+      private static readonly char delimiter = ';';
+      private string _recipients;
+      public int Id { get; set; }
+      public string Subject { get; set; }
+      public string Body { get; set; }
+      
+      public DateTime Datecreate { get; set; }
+      public string Result { get; set; }
+
+      [NotMapped]
+      public string[] Recipients
+      {
+         get => _recipients?.Split(delimiter);
+         set => _recipients = value != null ? string.Join($"{delimiter}", value) : null;
+      }
+   }
 }
