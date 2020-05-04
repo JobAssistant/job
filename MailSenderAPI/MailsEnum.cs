3b991522a66e5992a3e91322269f79dd61fd458b
xoste49 <xoste49@gmail.com>
2020-05-04 15:40:42 +0300

    Удалены старые файлы

--- MailSenderAPI/MailsEnum.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MailSenderAPI
-{
-   public class MailsEnum
-   {
-      public string subject { get; set; }
-      public string body { get; set; }
-      public string[] recipients { get; set; }
-   }
-}
